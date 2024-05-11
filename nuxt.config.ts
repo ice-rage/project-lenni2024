f5e739c78dba2e1584812ba36131b6108a0af989
ice-rage <otulissa2012@yandex.ru>
2024-05-11 12:57:28 +0700

    Дополнительные настройки проекта

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import svgLoader from "vite-svg-loader";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: ["@/styles/styles.less"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `
+            @import "@/styles/variables.less";
+            @import "@/styles/mixins.less";
+          `,
+        }
+      },
+    },
+    plugins: [
+      svgLoader(),
+    ],
+  },
 })
