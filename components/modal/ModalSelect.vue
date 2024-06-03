09b23d46b06c1d7eb4d7e2fdd7c559ae4ff1f2ba
ice-rage <otulissa2012@yandex.ru>
2024-06-03 21:40:16 +0700

    Добавлен плагин для кастомизации селекта

--- components/modal/ModalSelect.vue
+++ components/modal/ModalSelect.vue
@@ -1,38 +1,57 @@
 <template>
-  <select 
-    v-if="options.length" 
-    name="amount" 
-    class="field__select js-selectric"
-  >
-    <option 
-      v-for="(option, optionIndex) in options" 
-      :key="optionIndex"
-      :value="option.value"
-    >
-      {{ option.label }}
-    </option>
-  </select>
+  <VueSelect 
+    v-if="selectOptions.length" 
+    class="field__select select"
+    v-model="selected" 
+    placeholder=""
+    :options="selectOptions"
+    :isClearable="false"
+  />
 </template>
 
 <script setup>
-  const options = [
+  import "vue3-select-component/dist/style.css";
+
+  import VueSelect from "vue3-select-component";
+
+  const selected = ref("");
+  
+  const selectOptions = [
     {
-      value: "20",
       label: "до 20 человек",
+      value: "20",
     },
     {
-      value: "100",
       label: "от 20 до 100 человек",
+      value: "100",
     },
     {
-      value: "500",
       label: "от 100 до 500 человек",
+      value: "500",
     },
     {
-      value: "1000",
       label: "более 500 человек",
+      value: "1000",
     },
   ];
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+  .select {
+   
+    .control {
+      border: none;
+      width: 100%; 
+      padding: 0 15px;
+    }
+
+    .dropdown-icon {
+      width: 24px;
+      height: 24px;
+    }
+
+    .menu {
+      margin-top: 47px;
+    }
+  }
+</style>
