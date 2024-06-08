641ab771e8a3ab5c5418510863152f6760a94a6f
ice-rage <otulissa2012@yandex.ru>
2024-06-08 16:15:01 +0700

    Добавлены фавиконки

--- app.vue
+++ app.vue
@@ -20,11 +20,118 @@
   useHead({
     meta: [
       {
+        charset: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no",
+      },
+      {
+        name: "description",
+        content: "Арт—пространство для людей, горящих сердцем и делом",
+      },
+      {
+        name: "keywords",
+        content: "Lenni Art, пространство, мероприятия",
+      },
+      {
+        property: "og:title",
+        content: "Lenni Art",
+      },
+      {
+        property: "og:description",
+        content: "Арт—пространство для людей, горящих сердцем и делом",
+      },
+      {
+        property: "og:image",
+        content: "/images/lenni.png",
+      },
+      {
+        property: "og:type",
+        content: "website",
+      },
+      {
+        property: "og:url",
+        content: "https://lenni-art.ru/",
+      },
+      {
+        name: "msapplication-TileColor",
+        content: "#ffffff",
+      },
+      {
+        name: "msapplication-TileImage",
+        content: "/favicons/mstile-144x144.png",
+      },
+      {
+        name: "msapplication-Config",
+        content: "/favicons/browserconfig.xml",
+      },
+      {
+        name: "theme-color",
+        content: "#ffffff",
+      },
+      {
         name: "format-detection",
         content: "telephone=no",
       },
     ],
     link: [
+      {
+        rel: "shortcut icon",
+        href: "/favicons/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicons/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicons/favicon-32x32.png",
+      },
+      {
+        ref: "mask-icon",
+        href: "/favicons/safari-pinned-tab.svg",
+        color: "#bc3324",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/favicons/apple-touch-icon-60x60.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/favicons/apple-touch-icon-76x76.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/favicons/apple-touch-icon-120x120.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/favicons/apple-touch-icon-152x152.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicons/apple-touch-icon-180x180.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/favicons/android-chrome-192x192.png",
+      },
+      {
+        rel: "manifest",
+        href: "/favicons/site.webmanifest",
+      },
       {
         rel: "preconnect", 
         href: "https://fonts.googleapis.com",
